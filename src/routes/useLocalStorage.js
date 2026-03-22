a1abcfd99f51926fd98c8c44cd95ea9ef9c92b27
MiltonCoronado <miltonext@hotmail.com>
2026-03-22 00:06:49 -0500

    style: shutup linter

--- src/routes/useLocalStorage.js
+++ src/routes/useLocalStorage.js
@@ -1,51 +1,53 @@
 import { useEffect, useReducer } from 'react';
 
-const useLocalStorage = (itemName, initialValue) => {//Estos; son PARAMETROS, no PROPS!!!
+const useLocalStorage = (itemName, initialValue) => {
+  //Estos; son PARAMETROS, no PROPS!!!
   const [state, dispatch] = useReducer(reducer, initialState({ initialValue }));
 
-  const {sincronizedItem, error, loading, item} = state;
+  const { sincronizedItem, error, loading, item } = state;
 
-  const onError = (error) => dispatch({ 
-    type: actionTypes.error,
-    payload: error, 
-  });
+  const onError = (error) =>
+    dispatch({
+      type: actionTypes.error,
+      payload: error,
+    });
 
-  const onSuccess = (parsedItem) => dispatch({
-    type: actionTypes.success,
-    payload: parsedItem,
-  });
+  const onSuccess = (parsedItem) =>
+    dispatch({
+      type: actionTypes.success,
+      payload: parsedItem,
+    });
 
-  const onSave = (newItem) => dispatch({
-    type: actionTypes.save,
-    payload: newItem,
-  });
+  const onSave = (newItem) =>
+    dispatch({
+      type: actionTypes.save,
+      payload: newItem,
+    });
 
-  const onSincronize = () => dispatch({
-    type: actionTypes.sincronize,
-  });
-
+  const onSincronize = () =>
+    dispatch({
+      type: actionTypes.sincronize,
+    });
 
   useEffect(() => {
     setTimeout(() => {
       try {
-        const localStorageItem = localStorage.getItem(itemName);//"localStorage" solo maneja "strings" no estructuras de datos complejas, por ello se tienen que convertir primero en strings antes de guardarlas en el localStorage.
+        const localStorageItem = localStorage.getItem(itemName); //"localStorage" solo maneja "strings" no estructuras de datos complejas, por ello se tienen que convertir primero en strings antes de guardarlas en el localStorage.
         let parsedItem;
-        
-        if (!localStorageItem) {
-          localStorage.setItem(itemName, JSON.stringify(initialValue));//JSON.stringify(): this método convierte un objeto JavaScript o un valor en una cadena de texto JSON. Puede manejar objetos, arrays, strings, números, booleanos y null.
-        } else {
-          parsedItem = JSON.parse(localStorageItem);//JSON.parse(): Este método analiza una cadena de texto JSON y la convierte en un objeto JavaScript correspondiente al valor o estructura de datos descritos en la cadena JSON.
-        };
-        
-        console.log('useEffect(useLocalStorage) reinisializado porque se monto y desmonto el componente de ruta')
-        onSuccess(parsedItem);
-        
-      } catch(error) {
-        onError(error);
-      };
 
-    }, 2000)
-  },[sincronizedItem]);
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue)); //JSON.stringify(): this método convierte un objeto JavaScript o un valor en una cadena de texto JSON. Puede manejar objetos, arrays, strings, números, booleanos y null.
+        } else {
+          parsedItem = JSON.parse(localStorageItem); //JSON.parse(): Este método analiza una cadena de texto JSON y la convierte en un objeto JavaScript correspondiente al valor o estructura de datos descritos en la cadena JSON.
+        }
+
+        onSuccess(parsedItem);
+      } catch (error) {
+        onError(error);
+      }
+    }, 2000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sincronizedItem]);
 
   const sincronizeItem = () => {
     onSincronize();
@@ -56,15 +58,15 @@ const useLocalStorage = (itemName, initialValue) => {//Estos; son PARAMETROS, no
       const stringifiedItem = JSON.stringify(newItem);
       localStorage.setItem(itemName, stringifiedItem);
       onSave(newItem);
-    }catch(error){
+    } catch (error) {
       onError(error);
     }
   };
 
   return {
     item,
-    saveActionStorage, 
-    loading, 
+    saveActionStorage,
+    loading,
     error,
     sincronizeItem,
   };
@@ -74,7 +76,7 @@ const initialState = ({ initialValue }) => ({
   sincronizedItem: true,
   error: false,
   loading: true,
-  item: initialValue
+  item: initialValue,
 });
 
 const actionTypes = {
@@ -85,13 +87,13 @@ const actionTypes = {
 };
 
 const reducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case actionTypes.error:
       return {
         ...state,
         loading: false,
         error: true,
-      }
+      };
     case actionTypes.success:
       return {
         ...state,
@@ -99,25 +101,24 @@ const reducer = (state, action) => {
         loading: false,
         sincronizedItem: true,
         item: action.payload,
-      }
+      };
     case actionTypes.save:
       return {
         ...state,
-        item: action.payload
-      } 
+        item: action.payload,
+      };
     case actionTypes.sincronize:
       return {
         ...state,
         loading: true,
         sincronizedItem: false,
-      }
+      };
     default:
-      return initialState;   
+      return initialState;
   }
 };
 
 export { useLocalStorage };
-
 
 // const defaultTodos = [
 //   {text: 'Cortar cebolla', completed: true},
